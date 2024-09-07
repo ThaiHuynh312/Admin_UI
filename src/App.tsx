25bf09e6ba9c47621bb03038aa0a6ed14b6cd87f
ThaiHuynh312 <thaihuynh31220030913@gmail.com>
2024-09-07 08:00:55 +0700

    feat:[UI][INIT] setup env

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Box from '@mui/material/Box'
+import Button from '@mui/material/Button'
+
+function App() {
+  return (
+    <Box>
+      <Button variant='outlined'> Hello</Button>
+    </Box>
+  )
+}
+
+export default App
